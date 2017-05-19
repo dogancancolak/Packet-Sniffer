69669790ba57fa9c64703e94da8cbce3f72c15f1
dogancan <dogancancolak1@gmail.com>
2017-05-19 23:18:27 +0300

    Counters added & UI modified & MAC address added

--- sniffer.py
+++ sniffer.py
@@ -1,63 +1,49 @@
-# Packet sniffer in python for Linux
-# Sniffs only incoming TCP packet
+# Packet sniffer in python
+# For Linux - Sniffs all incoming and outgoing packets
 
-import socket, sys, textwrap
+import socket, sys
 from struct import *
-import sys
-
-
-# create an INET, STREAMing socket
+packet_counter = 0
+tcp_counter = 0
 
 # Convert a string of 6 characters of ethernet address into a dash separated hex string
 def eth_addr(a):
-    b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (a[0], a[1], a[2], a[3], a[4], a[5])
-    return b
+    # formatting each chunk to decimal addresses
+    # and returns properly formatted mac address
+    bytes_str = map('{:02x}'.format, a)
+    return ':'.join(bytes_str).upper()
 
 
 # create a AF_PACKET type raw socket (thats basically packet level)
-# define ETH_P_ALL    0x0003          /* Every packet (be careful!!!) */
+# define ETH_P_ALL    0x0003          /* Every packet */
 try:
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
-except (socket.error):
+except socket.error:
     print('Socket could not be created.')
     sys.exit()
-
-
-def format_multi_line(string, size=80):
-    if isinstance(string, bytes):
-        string = ''.join(map(chr, string))
-        # string = ''.join(r'\x{:02}'.format(byte) for byte in string)
-        if size % 2:
-            size -= 1
-    return '\n'.join([line for line in textwrap.wrap(string, size)])
-
 
 # receive a packet
 while True:
     # TCP/IP can handle packets of maximum legal size 65,565 bytes
     packet = s.recvfrom(65565)
-
+    packet_counter = packet_counter+1
     # taking string form of packet from tuple
     packet = packet[0]
 
-    # parse ethernet header!
+    # parse ethernet header
     eth_length = 14
 
+    # Taking packet's first 14 character for ethernet header
     eth_header = packet[:eth_length]
     eth = unpack('!6s6sH', eth_header)
     eth_protocol = socket.ntohs(eth[2])
-
-    print('\nDestination MAC : ' + eth_addr(packet[0:6]) + ' Source MAC : ' + eth_addr(
-        packet[6:12]) + ' Protocol : ' + str(eth_protocol))
+    print('Destination MAC : ' + eth_addr(packet[0:6]) + ' || Source MAC : ' + eth_addr(packet[6:12]) + ' || Ethernet Protocol : ' + str(eth_protocol))
 
     # Parse IPv4 packets, Ethertype number = 0x0800
     if eth_protocol == 8:
-
         # Parse IP header
         # take first 20 characters for the ip header
         ip_header = packet[eth_length:20 + eth_length]
-        # take first 20 characters for the ip header
-        ip_header = packet[0:20]
 
         # formatting the header we have with :
         # ! = big-endian
@@ -85,39 +71,46 @@ while True:
         ttl = iph[5]
 
         protocol = iph[6]
-        str_protocol = "" + str(protocol)
-        if protocol == 6:  # protocol 6 means TCP
-            str_protocol = str_protocol + " (TCP)"
+        source_addr = socket.inet_ntoa(iph[8]);
+        destination_addr = socket.inet_ntoa(iph[9]);
 
-        source_addr = socket.inet_ntoa(iph[8])
-        destination_addr = socket.inet_ntoa(iph[9])
+        print('Version : ' + str(version) + ' || IP Header Length : ' + str(ihl) + ' || TTL : ' + str(ttl) + ' || Protocol : ' + str(protocol) + ' || Source Address : ' + str(source_addr) + ' || Destination Address : ' + str(destination_addr))
 
-        print('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(
-            ttl) + ' Protocol : ' + str_protocol + '\nSource Address : ' + str(
-            source_addr) + ' Destination Address : ' + str(destination_addr))
+        # TCP protocol
+        if protocol == 6:
 
-        # after ip header , tcp header comes. we take packets 20 characters from the begining of tcp header.
-        tcp_header = packet[ipheader_len:ipheader_len + 20]
+            # another tcp packet captured
+            tcp_counter = tcp_counter+1
 
-        # Now its time o unpack the tcp header like we did before to ip header. But this time with diferent format.
-        tcph = unpack('!HHLLBBHHH', tcp_header)
+            # after ip header , tcp header comes. we take packet's 20 characters from the begining of tcp header.
+            t = ipheader_len + eth_length
+            tcp_header = packet[t:t + 20]
 
-        # in tcp headers structure
-        source_port = tcph[0]  # first element is source port
-        dest_port = tcph[1]  # second element is destination port
-        sequence = tcph[2]  # third element is Sequence number
-        acknowledgement = tcph[3]
-        doff_reserved = tcph[4]
-        tcph_length = doff_reserved >> 4
+            # Now its time o unpack the tcp header like we did before to ip header. But this time with diferent format.
+            tcph = unpack('!HHLLBBHHH', tcp_header)
 
-        print('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + '\nSequence Number : ' + str(
-            sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length))
+            # in tcp headers structure
+            source_port = tcph[0]  # first element is source port
+            dest_port = tcph[1]  # second element is destination port
+            sequence = tcph[2]  # third element is Sequence number
+            acknowledgement = tcph[3]  # fourth element is ack.
+            tcph_length = tcph[4] >> 4
 
-        h_size = ipheader_len + tcph_length * 4
+            print('Source Port : ' + str(source_port) + ' || Destination Port : ' + str(dest_port) + ' || Sequence Number : ' + str(sequence) + ' || Acknowledgement : ' + str(acknowledgement) + ' || TCP header length : ' + str(tcph_length))
 
-        # get data from the packet
-        data = packet[h_size:]
-        data = ''.join(map(chr, data))
+            h_size = (eth_length + ipheader_len + tcph_length) * 4
+            data_size = len(packet) - h_size
 
-        print('Data : ' + data)
+            # get data from the packet
+            data = packet[h_size:]
+            data = ''.join(map(chr, data))
+
+            print('Data : ' + data)
+
+        # some other IP packet like IGMP or ICMP or UDP ...
+        else:
+            print('Protocol other than TCP')
+
+        print()
+        print('Captured packets : ' + str(packet_counter) + '  TCP Packets : '+ str(tcp_counter))
         print()
